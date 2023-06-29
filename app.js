5692b30c04385346f1abdff2a14a36215487a9cd
Zou <lisheng.zou@sandc.com>
2023-06-29 16:08:21 -0500

    0629

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-d3.select('div')
-    .selectAll('p')
-    .data([1,2,3])
-    .enter()
-    .append('p')
-    .text(dta => dta)
+const DUMMY_DATA = [
+    {id: 'd1', value: 10, region: 'USA'},
+    {id: 'd2', value: 11, region: 'India'},
+    {id: 'd3', value: 12, region: 'China'},
+    {id: 'd4', value: 6, region: 'Germany'}
+]
 
-
+const container =  d3.select('svg')
+    .classed('container', true)
+    .style('border', '1px solid red');
     ;
+
+container
+    .selectAll('.bar')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', 50)
+    .attr('height', data => data.value * 15)
+
+
+
+//Note
+//    .selectAll('p')
+//    .data(DUMMY_DATA)
+//    .enter()
+//    .append('p')
+//    .text(d=>d)
+
+
